719b54065480ee521de4f6a9308ca5480cf88c2f
vladimirtischler <tischler.vladimir@gmail.com>
2019-12-04 22:07:11 +0100

    vlado

--- Open-Lab-04.08/StringTools.cs
+++ Open-Lab-04.08/StringTools.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Open_Lab_04._08
 {
@@ -6,7 +7,16 @@ namespace Open_Lab_04._08
     {
         public string[] IsFourLetters(string[] strings)
         {
-            throw new NotImplementedException();
+            List<string> word = new List<string>();
+            foreach(var c in strings)
+            {
+                if(c.Length == 4)
+                {
+                    word.Add(c);
+                }
+
+            }
+            return word.ToArray();
         }
     }
 }
